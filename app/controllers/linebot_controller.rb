181a93bb642968259fe54329abb673a1f48893ca
unknown <shotakosan.script@gmail.com>
2020-11-26 21:05:52 +0900

    change

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,4 +1,4 @@
-class LinebotController < ApplicationController
+class LinebotController < ActionController::Base
     require 'line/bot'  # gem 'line-bot-api'
   
     # callbackアクションのCSRFトークン認証を無効
