b547492d3f37b60f661fa304c46c58220a0b363b
Shotaro-pd <shotakosan.script@gmail.com>
2020-12-10 06:21:05 +0000

    Route修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-        get '/board' => 'board#index'
-        get '/ranking' => 'board#rank'
-        # resources :board, only:[:index]
-        # resources :ranking, only[:rank]
+        resources :board do
+            collection do
+                get 'index'
+                get 'rank'
+            end
+        end
     end
   end
 
