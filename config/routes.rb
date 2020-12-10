7a1d2d5cf0021b28663827cb811a0eefb8732ea4
Shotaro-pd <shotakosan.script@gmail.com>
2020-12-10 06:32:13 +0000

    Route修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-        resources :board, only:[:index, :rank]
+        resources :board do
+            post 'rank', on: :collection
+        end
     end
   end
 
